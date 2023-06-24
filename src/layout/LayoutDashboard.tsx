6a01f2f0334c5897af3e124c6fe800b2bc4e7fef
Tran Hoang Long <hoanglongvh2000@gmail.com>
2023-06-24 19:17:56 +0700

    update_design_posting

--- src/layout/LayoutDashboard.tsx
+++ src/layout/LayoutDashboard.tsx
@@ -5,42 +5,42 @@ import { Container } from "components/common";
 import React from "react";
 
 interface LayoutDashboardProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export const LayoutDashboard: React.FC<LayoutDashboardProps> = ({
-  children,
+    children,
 }: LayoutDashboardProps) => {
-  return (
-    <React.Fragment>
-      <Header/>
-      <main className="h-full py-8">
-        <Container>
-          <nav
-            className="hidden sm:flex flex-col sm:w-2/12 md:w-3/12 h-full max-w-[230px]"
-            aria-label="Primary"
-          >
-            <SideBar />
-          </nav>
-          <section
-            className="w-full h-[605px] sm:w-10/12 md:w-6/12 overflow-y-scroll scrollbar-hide z-0 sm:px-5"
-            role="main"
-          >
-            {children}
-          </section>
-          <section className="flex-col hidden w-3/12 space-y-5 md:flex"></section>
+    return (
+        <React.Fragment>
+            <Header />
+            <main className="h-full py-8">
+                <Container>
+                    <nav
+                        className="hidden sm:flex flex-col sm:w-2/12 md:w-3/12 h-[705px] max-w-[230px]"
+                        aria-label="Primary"
+                    >
+                        <SideBar />
+                    </nav>
+                    <section
+                        className="w-full  sm:w-10/12 md:w-6/12 overflow-y-scroll scrollbar-hide z-0 sm:px-5"
+                        role="main"
+                    >
+                        {children}
+                    </section>
+                    <section className="flex-col hidden w-3/12 space-y-5 md:flex"></section>
 
-          {/* SHOWED ONLY ON SMALL DEVICE */}
-          <section
-            className="fixed bottom-0 left-0 right-0 z-10 w-full bg-white dark:bg-bg-dark sm:hidden"
-            role="navigation"
-            aria-label="Secondary"
-          ></section>
-          <div className="absolute"></div>
-        </Container>
-      </main>
-    </React.Fragment>
-  );
+                    {/* SHOWED ONLY ON SMALL DEVICE */}
+                    <section
+                        className="fixed bottom-0 left-0 right-0 z-10 w-full bg-white dark:bg-bg-dark sm:hidden"
+                        role="navigation"
+                        aria-label="Secondary"
+                    ></section>
+                    <div className="absolute"></div>
+                </Container>
+            </main>
+        </React.Fragment>
+    );
 };
 
 export default LayoutDashboard;
--- src/pages/PostAddNew.tsx
+++ src/pages/PostAddNew.tsx
@@ -1,54 +1,93 @@
 import LayoutShared from "layout/LayoutDashboard";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill";
 
 export const PostAddNew: React.FC = () => {
     const { register, handleSubmit } = useForm();
-
+    const [content, setContent] = useState("");
     const onSubmit = (data: any) => {
         console.log(data);
     };
+    const handleContentChange = (value: any) => {
+        setContent(value);
+    };
+    const toolbarOptions = [
+        ["bold", "italic", "underline", "strike"],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [{ font: [] }],
+        ["blockquote", "code-block"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ indent: "-1" }, { indent: "+1" }],
+        ["link", "image"],
+        ["clean"],
+    ];
 
     return (
         <LayoutShared>
-            <div className="container mx-auto">
-                <h1 className="text-2xl font-bold mb-4">Add new post</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="mb-4">
-                        <label
-                            htmlFor="title"
-                            className="block font-semibold mb-1"
-                        >
-                            Title
-                        </label>
-                        <input
-                            type="text"
-                            id="title"
-                            className="w-full border border-gray-300 rounded px-3 py-2"
-                            {...register("title")}
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label
-                            htmlFor="content"
-                            className="block font-semibold mb-1"
-                        >
-                            Content
-                        </label>
-                        <textarea
-                            id="content"
-                            className="w-full border border-gray-300 rounded px-3 py-2"
-                            rows={4}
-                            {...register("content")}
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="bg-blue-500 text-white rounded px-4 py-2"
-                    >
-                        Add post
-                    </button>
-                </form>
+            <div className="container mx-auto h-auto">
+                <div className="sticky top-0 rounded-lg z-10 p-4 text-white bg-blue-600 shadow-md">
+                    <h1 className="text-2xl font-bold ">Add new post</h1>
+                </div>
+                <div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="mb-4 mt-4">
+                            <label
+                                htmlFor="title"
+                                className="block font-semibold mb-1 text-base"
+                            >
+                                Title
+                            </label>
+                            <input
+                                type="text"
+                                id="title"
+                                className="w-full border border-gray-300 rounded px-3 py-2"
+                                {...register("title")}
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label
+                                htmlFor="hastag"
+                                className="block font-semibold mb-1 text-base"
+                            >
+                                Hastag
+                            </label>
+                            <input
+                                type="text"
+                                id="hastag"
+                                className="w-full border border-gray-300 rounded px-3 py-2"
+                                {...register("hastag")}
+                            />
+                        </div>
+                        <div className=" h-80 ">
+                            <label
+                                htmlFor="content"
+                                className="block font-semibold mb-1 text-base"
+                            >
+                                Content
+                            </label>
+                            <div className="overflow-auto">
+                                <ReactQuill
+                                    className="border-gray-300 w-full mb-4"
+                                    value={content}
+                                    onChange={handleContentChange}
+                                    modules={{
+                                        toolbar: toolbarOptions,
+                                    }}
+                                />
+                            </div>
+                        </div>
+                        <div className="mt-6">
+                            <button
+                                type="submit"
+                                className="bg-blue-500 text-white rounded px-4 py-2"
+                            >
+                                Add post
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </LayoutShared>
     );
