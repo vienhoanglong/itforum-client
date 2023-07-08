25efe408c87022b4a4677ae490c0ab2dd4b16963
Vien <vienhoanglong789@gmail.com>
2023-07-08 17:33:21 +0700

    chore: add ui group chat information and style chat page

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,12 +1,25 @@
 import LayoutChat from "@/layout/LayoutChat";
 import React from "react";
-import { HiInformationCircle, HiMenu, HiOutlineSearch } from "react-icons/hi";
+import {
+  HiInformationCircle,
+  HiLogout,
+  HiMenu,
+  HiOutlineSearch,
+  HiOutlineSparkles,
+} from "react-icons/hi";
 import { AvatarImage } from "@/components/image";
 import ChatBox from "@/modules/chat/ChatBox";
 import avt1 from "@/assets/avt1.jpg";
 import avt2 from "@/assets/avt2.jpg";
 import Avatar from "@/components/image/Avatar";
-import { BsFillPlusCircleFill } from "react-icons/bs";
+import {
+  BsFileEarmarkFill,
+  BsFillImageFill,
+  BsFillPlusCircleFill,
+  BsLink45Deg,
+  BsPinAngle,
+} from "react-icons/bs";
+import { FaUserFriends } from "react-icons/fa";
 export const ChatPage: React.FC = () => {
   const users = [
     { id: 1, username: "Viên Hoàng Long" },
@@ -16,6 +29,10 @@ export const ChatPage: React.FC = () => {
   const handleMenuChat = () => {
     setShowMenuChat(!showMenuChat);
   };
+  const [activeTabChatInfo, setActiveTabChatInfo] = React.useState<string>("file")
+  const handleTabChatClick = (tab: string) => {
+    setActiveTabChatInfo(tab);
+  }
   return (
     <LayoutChat>
       <div className="bg-white p-0 rounded-lg lg:max-w-960 md:max-w-720 sm:max-w-540">
@@ -25,14 +42,29 @@ export const ChatPage: React.FC = () => {
             <h5 className="text-sm mb-2">Friends</h5>
             <div className="flex flex-row gap-1">
               <div className="relative">
-                <img src={avt1} className="h-10 w-10 object-cover border border-dark4 rounded-full" />
-                <BsFillPlusCircleFill className="text-xs absolute bottom-0 right-1 text-mainColor"/>
+                <img
+                  src={avt1}
+                  className="h-10 w-10 object-cover border border-dark4 rounded-full"
+                />
+                <BsFillPlusCircleFill className="text-xs absolute bottom-0 right-1 text-mainColor" />
               </div>
               <div className="items-center flex flex-row gap-2 justify-between">
-                <Avatar src={avt1} cln="h-10 w-10 object-cover border border-dark4"></Avatar>
-                <Avatar src={avt2} cln="h-10 w-10 object-cover border border-dark4"></Avatar>
-                <Avatar src={avt1} cln="h-10 w-10 object-cover border border-dark4"></Avatar>
-                <Avatar src={avt2} cln="h-10 w-10 object-cover border border-dark4"></Avatar>
+                <Avatar
+                  src={avt1}
+                  cln="h-10 w-10 object-cover border border-dark4"
+                ></Avatar>
+                <Avatar
+                  src={avt2}
+                  cln="h-10 w-10 object-cover border border-dark4"
+                ></Avatar>
+                <Avatar
+                  src={avt1}
+                  cln="h-10 w-10 object-cover border border-dark4"
+                ></Avatar>
+                <Avatar
+                  src={avt2}
+                  cln="h-10 w-10 object-cover border border-dark4"
+                ></Avatar>
               </div>
             </div>
           </div>
@@ -138,8 +170,10 @@ export const ChatPage: React.FC = () => {
               showMenuChat ? "block" : "hidden"
             }`}
           >
-            <div className="flex items-center align-middle flex-col">
-              <h3 className="mt-5 text-lg">Group Chat</h3>
+            <div className="flex items-center flex-col">
+              <h3 className="mt-5 text-md text-dark0">
+                Group chat information
+              </h3>
               <div className="relative mx-auto w-[80px] h-[80px] mt-2">
                 <Avatar
                   src={avt2}
@@ -151,6 +185,150 @@ export const ChatPage: React.FC = () => {
                   alt="Image 2"
                   cln="h-14 w-14 rounded-full ring-2 ring-white absolute bottom-0 right-0 object-cover border"
                 />
+              </div>
+              <div className="flex justify-between gap-4 mt-4">
+                <div className="flex p-2 border-[1px] items-center flex-col rounded-lg min-w-[30%] bg-light3 cursor-pointer hover:text-darker hover:font-bold">
+                  <BsPinAngle />
+                  <span className="text-xs mt-1 font-medium">Pin</span>
+                </div>
+                <div className="flex p-2 border-[1px] items-center flex-col rounded-lg min-w-[30%] bg-light3 cursor-pointer hover:text-darker hover:font-bold">
+                  <HiOutlineSparkles />
+                  <span className="text-xs mt-1 font-medium">Theme</span>
+                </div>
+                <div className="flex p-2 border-[1px] items-center flex-col rounded-lg min-w-[30%] bg-light3 cursor-pointer hover:text-darker hover:font-bold">
+                  <HiLogout />
+                  <span className="text-xs mt-1 font-medium">Leave</span>
+                </div>
+              </div>
+              <div className="mt-5 border-t-2 w-full p-2">
+                <div className="flex flex-row justify-between">
+                  <div className="flex flex-row gap-2 items-center">
+                    <FaUserFriends className="text-lg" />
+                    <div className="flex gap-2">
+                      <span className="text-sm">Member</span>
+                      <span className="text-mainColor bg-subtle p-[2px_6px] rounded-md text-xs">
+                        4
+                      </span>
+                    </div>
+                  </div>
+                  <span className="text-xs font-medium">Show all</span>
+                </div>
+                <div className="flex flex-col mt-3 gap-2">
+                  <div className="flex gap-2">
+                    <Avatar
+                      src={avt2}
+                      cln="h-10 w-10 object-cover border border-dark4"
+                    ></Avatar>
+                    <div>
+                      <span className="text-sm">Viên Hoàng Long</span>
+                      <p className="text-xs">Quản trị viên</p>
+                    </div>
+                  </div>
+                  <div className="flex gap-2">
+                    <Avatar
+                      src={avt1}
+                      cln="h-10 w-10 object-cover border border-dark4"
+                    ></Avatar>
+                    <div>
+                      <span className="text-sm">Trần Hoàng Long</span>
+                      <p className="text-xs">Thành viên</p>
+                    </div>
+                  </div>
+                  <div className="flex gap-2">
+                    <Avatar
+                      src={avt1}
+                      cln="h-10 w-10 object-cover border border-dark4"
+                    ></Avatar>
+                    <div>
+                      <span className="text-sm">Trần Hoàng Long</span>
+                      <p className="text-xs">Thành viên</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full mt-1">
+                <ul className="flex gap-2 justify-center">
+                  <li className={`block p-1 text-sm font-medium text-center border min-w-[30%] rounded-lg cursor-pointer hover:font-bold ${activeTabChatInfo === "file" ? 'bg-mainColor text-light1' : ''}`} onClick={()=>handleTabChatClick("file")}>
+                    File
+                  </li>
+                  <li className={`block p-1 text-sm font-medium text-center border min-w-[30%] rounded-lg cursor-pointer hover:font-bold ${activeTabChatInfo === "media" ? 'bg-mainColor text-light1' : ''}`} onClick={()=>handleTabChatClick("media")}>
+                    Media
+                  </li>
+                  <li className={`block p-1 text-sm font-medium text-center border min-w-[30%] rounded-lg cursor-pointer hover:font-bold ${activeTabChatInfo === "link" ? 'bg-mainColor text-light1' : ''}`} onClick={()=>handleTabChatClick("link")}>
+                    Link
+                  </li>
+                </ul>
+                <div className="p-2">
+                  { activeTabChatInfo === "media" && <div className="1">
+                    <div className="flex gap-2 mb-1">
+                      <div className="p-3 border rounded-md bg-[#fafafa] w-fit">
+                        <BsFillImageFill className="text-sm text-dark3" />
+                      </div>
+                      <div className="">
+                        <span className="text-xs">Ảnh mẫu.png</span>
+                        <p className="text-[10px]">Sun 8 2023</p>
+                      </div>
+                    </div>
+                    <div className="flex gap-2 mb-1">
+                      <div className="p-3 border rounded-md bg-[#fafafa] w-fit">
+                        <BsFillImageFill className="text-sm text-dark3" />
+                      </div>
+                      <div className="">
+                        <span className="text-xs">Ảnh mẫu.png</span>
+                        <p className="text-[10px]">Sun 8 2023</p>
+                      </div>
+                    </div>
+                    <div className="flex gap-2 mb-1">
+                      <div className="p-3 border rounded-md bg-[#fafafa] w-fit">
+                        <BsFillImageFill className="text-sm text-dark3" />
+                      </div>
+                      <div className="">
+                        <span className="text-xs">Ảnh mẫu.png</span>
+                        <p className="text-[10px]">Sun 8 2023</p>
+                      </div>
+                    </div>
+                  </div>}
+                  {activeTabChatInfo === "file" && <div className="2">
+                    <div className="flex gap-2 mb-1">
+                      <div className="p-3 border rounded-md bg-[#fafafa] w-fit">
+                        <BsFileEarmarkFill className="text-sm text-dark3" />
+                      </div>
+                      <div className="">
+                        <span className="text-xs">Book1.zip</span>
+                        <p className="text-[10px]">Sun 8 2023</p>
+                      </div>
+                    </div>
+                    <div className="flex gap-2 mb-1">
+                      <div className="p-3 border rounded-md bg-[#fafafa] w-fit">
+                        <BsFileEarmarkFill className="text-sm text-dark3" />
+                      </div>
+                      <div className="">
+                        <span className="text-xs">Book1.zip</span>
+                        <p className="text-[10px]">Sun 8 2023</p>
+                      </div>
+                    </div>
+                  </div>}
+                  {activeTabChatInfo === "link" && <div className="2">
+                    <div className="flex gap-2 mb-1">
+                      <div className="p-3 border rounded-md bg-[#fafafa] w-fit">
+                        <BsLink45Deg className="text-sm text-dark3" />
+                      </div>
+                      <div className="">
+                        <span className="text-xs">vienhoanglong.com</span>
+                        <p className="text-[10px]">Sun 8 2023</p>
+                      </div>
+                    </div>
+                    <div className="flex gap-2 mb-1">
+                      <div className="p-3 border rounded-md bg-[#fafafa] w-fit">
+                        <BsLink45Deg className="text-sm text-dark3" />
+                      </div>
+                      <div className="">
+                        <span className="text-xs">vienhoanglong.com</span>
+                        <p className="text-[10px]">Sun 8 2023</p>
+                      </div>
+                    </div>
+                  </div>}
+                </div>
               </div>
             </div>
           </div>
