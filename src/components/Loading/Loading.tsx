3682bb2555ccda5596d6fe0e627ddb7e2f0ee35a
Vien <vienhoanglong789@gmail.com>
2023-09-04 01:34:46 +0700

    fix: loading SPA

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Loading: React.FC = () => {
   return (
-    <div className="flex flex-col items-center gap-2">
-      <div className="w-8 h-8 border-2 border-slate-300 rounded-full animate-spin border-t-transparent"></div>
-      <span className="animate-bounce text-slate-300 leading-3 font-light">
+    <div className="flex flex-col items-center gap-2 m-auto">
+      <div className="w-8 h-8 border-2 border-mainColor rounded-full animate-spin border-t-transparent"></div>
+      <span className="animate-bounce text-mainColor leading-3 font-light">
         Loading...
       </span>
     </div>
